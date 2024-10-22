7bdb2bd3a3e6c4ec7764f5e79de3431173253508
OMPRATIK <routprateek4@gmail.com>
2024-10-22 23:53:53 +0530

    added project section

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,5 +1,30 @@
+import { GrProjects } from "react-icons/gr";
+import SubHeading from "../components/SubHeading";
+import { useState } from "react";
+
+const options = ["Front-end", "Back-end", "Full-stack"];
+
 function Projects() {
-  return <div>Projects</div>;
+  const [op, setOp] = useState("full-stack");
+  return (
+    <div className="mt-5 sm:mt-10">
+      <div>
+        <SubHeading heading={"Projects"} icon={<GrProjects />} />
+        <select
+          value={op}
+          onChange={(e) => setOp(e.target.value)}
+          className="rounded-md border-[1px] border-zinc-700 bg-zinc-800 px-2 py-1 text-zinc-400
+            focus:border-[1px] focus:border-zinc-700"
+        >
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
